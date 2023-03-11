c39ae849c150663ff279fb1c5728f2b399c619bf
11Olya112 <OlyaBushovska767@gmail.com>
2023-03-11 16:13:52 +0300

    solution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './Header.scss';
 
-interface ExchangeRate {
-  currencyCodeA: number;
-  currencyCodeB: number;
-  rateBuy: number;
-  rateSell: number;
-  date: number;
-}
-
 interface ExchangeRates {
   USD: number;
   EUR: number;
+}
+
+interface ExchangeRate {
+  cc: string;
+  rate: number;
 }
 
 export const Header: React.FC = () => {
   const [rates, setRates] = useState<ExchangeRates>({ USD: 0, EUR: 0 });
 
   useEffect(() => {
-    fetch('https://api.monobank.ua/bank/currency')
-      .then((response) => response.json())
-      .then((data: ExchangeRate[]) => {
-        const usdRate = data.find((item) => item
-          .currencyCodeA === 840 && item.currencyCodeB === 980);
-        const eurRate = data.find((item) => item
-          .currencyCodeA === 978 && item.currencyCodeB === 980);
+    const url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 
-        if (usdRate && eurRate) {
-          setRates({ USD: usdRate.rateBuy, EUR: eurRate.rateBuy });
-        }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const usdRate: number = data.find((rate: ExchangeRate) => rate.cc === 'USD').rate;
+        const eurRate: number = data.find((rate: ExchangeRate) => rate.cc === 'EUR').rate;
+
+        setRates({ USD: usdRate, EUR: eurRate });
       })
       .catch(error => error);
   }, []);
